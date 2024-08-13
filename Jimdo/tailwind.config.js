8c7e0631ddf71449ecbcd0964553b3ae0967598e
Arzel Serdeña <arzeldserdena@iskolarngbayan.pup.edu.ph>
2024-08-13 16:58:14 +0800

    header v1

--- Jimdo/tailwind.config.js
+++ Jimdo/tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "br-blue": "#003CBA",
+      "lt-blue": "#001453",
+      bodyWhiteDarker: "#F8F8F8",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
     extend: {},
   },
   plugins: [],
