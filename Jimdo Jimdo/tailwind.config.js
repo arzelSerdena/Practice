5283d43dcb81c71532becb4a6ce30b9940196a49
Arzel Serdeña <arzeldserdena@iskolarngbayan.pup.edu.ph>
2024-08-17 14:06:54 +0800

    home

---
+++ Jimdo Jimdo/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      "dark-blue": "#001453",
+      "bright-blue": "#004ced",
+      "dark-white": "#f8f8f8",
+      white: "#FFFFFF",
+      "gray-border": "#d5d5d5",
+      "gray-text": "#6d6d6d",
+      "faq-border": "#e6e6e6",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Euclid Circular A", "sans-serif"],
+      },
+      boxShadow: {
+        "custom-shadow": "0 2px 12px 0 rgba(0, 0, 0, 0.06)",
+      },
+    },
+  },
+  plugins: [],
+};
