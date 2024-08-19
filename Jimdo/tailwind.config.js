d66684cbbc72aeb6458678c2c139e224196b66df
Arzel Serdeña <arzeldserdena@iskolarngbayan.pup.edu.ph>
2024-08-19 08:18:24 +0800

    accordion, responsive navbar, dropdown, carousel initial

--- Jimdo/tailwind.config.js
+++ Jimdo/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       "gray-border": "#d5d5d5",
       "gray-text": "#6d6d6d",
       "faq-border": "#e6e6e6",
+      black: "#000000",
     },
     extend: {
       fontFamily: {
@@ -18,6 +19,13 @@ module.exports = {
       },
       boxShadow: {
         "custom-shadow": "0 2px 12px 0 rgba(0, 0, 0, 0.06)",
+      },
+
+      transitionProperty: {
+        "max-height": "max-height",
+      },
+      transitionDuration: {
+        1400: "1400ms",
       },
     },
   },
